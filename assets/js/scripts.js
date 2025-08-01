47d68eee04ca0e1770592743a0032b8d75e6c2b7
Luani Simtob <luani.simtob@gmail.com>
2025-08-01 19:30:12 -0300

    Formulário

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById('contactForm');
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      if (this.checkValidity()) {
+        document.getElementById('feedbackMsg').classList.remove('hidden');
+        this.reset();
+
+        setTimeout(() => {
+          document.getElementById('feedbackMsg').classList.add('hidden');
+        }, 4000);
+      }
+    });
+  }
+});
+
 function openModal(src) {
-    document.getElementById("modalImg").src = src;
-    document.getElementById("imageModal").classList.remove("hidden");
+  document.getElementById("modalImg").src = src;
+  document.getElementById("imageModal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("imageModal").classList.add("hidden");
+}
+
+function maskPhone(input) {
+  let value = input.value.replace(/\D/g, '');
+  if (value.length > 10) {
+    input.value = value.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  } else {
+    input.value = value.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3');
   }
-  function closeModal() {
-    document.getElementById("imageModal").classList.add("hidden");
-  }
+}
+
